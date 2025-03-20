1c1300496639de5dce3aac4b1220c7803ecdaf93
Vidish_Bajpai <kushbajpai641@gmail.com>
2025-03-20 19:53:02 +0530

    Add files via upload

---
+++ DBMS/upload_pets.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from pets_data import Data
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="PETS_DB"
+)
+cursor = connection.cursor()
+sql = 'INSERT INTO Pets(Species, Pet_Name, Breed, Age) VALUES (%s,%s,%s,%s)'
+
+for pet in Data:
+    val = (pet['Species'],pet['Pet_Name'],pet['Breed'],int(pet['Age']))
+    cursor.execute(sql,val)
+connection.commit()
+print('Job Done!')
+
+cursor.close()
+connection.close()
